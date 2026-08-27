df61c471d960c0faf14f955ebd8417c07d870994
jjeongy644 <kje1352@naver.com>
2026-08-27 11:10:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 st.set_page_config(page_title="조선대학교 추천채용 통합 관리 시스템", layout="wide")
 
 # --- 구글 앱스 스크립트 웹훅 연동 설정 (인증 에러 100% 원천 차단) ---
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwkRXMZXIueGOKflC-AGhcMwz3JrMzyRWa_S-QFYLQdHJJSAdwkRP2lM9ZAY3WkFE7OQw/exec"
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzW4PWanIZyOHTwZZ2Nso_TnYjOCBIS_Aj2WJ9fjF5TxBaljuXFVcbLVnJajKGoQyiLuA/exec"
 
 def load_data_from_gs(sheet_name, default_columns):
     try:
